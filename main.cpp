9302f3532b1d5bed133a99250bd57df20450d35b
PavloKostiv <73310882+PavloKostiv@users.noreply.github.com>
2020-10-22 22:11:37 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream f1;
+    f1.open("input.txt");
+    ofstream f2;
+    f2.open("output.txt");
+
+    int N,kil;
+    f1>>N;
+    int a[10];
+
+    for(int i=0; i<N; i++)
+       f1>>a[i];
+     for(int i=0; i<N; i++)
+       {
+          if(a[])
+       }
+
+
+    return 0;
+}
