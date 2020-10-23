0f9c657557017730378cf970887f9aca270dd015
PavloKostiv <73310882+PavloKostiv@users.noreply.github.com>
2020-10-23 15:17:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    ifstream f1;
+    f1.open("input.txt");
+    ofstream f2;
+    f2.open("output.txt");
+
+    f1>>a;
+
+    b=a%10;
+    c=(a-b)/10;
+
+    f2<<c<<" "<<b;
+
+    f2.close();
+    f1.close();
+    return 0;
+}
